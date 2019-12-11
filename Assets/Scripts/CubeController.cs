b08f9ba6a4647ec834b0ab7e1dc796fdc6801d22
natto17 <48194106+natto17@users.noreply.github.com>
2019-12-11 07:37:50 -0500

    final!

--- Assets/Scripts/CubeController.cs
+++ Assets/Scripts/CubeController.cs
@@ -8,6 +8,7 @@ public class CubeController : MonoBehaviour
     public int myX,myY;
     GameController myGameController;
     public bool active = false;
+    public int colorValue = 99;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +24,6 @@ public class CubeController : MonoBehaviour
     }
     void OnMouseDown()
     {
-        myGameController.ProcessClick(gameObject, myX, myY, gameObject.GetComponent<Renderer>().material.color, active);
+        myGameController.ProcessClick(gameObject, myX, myY, gameObject.GetComponent<Renderer>().material.color, active, colorValue);
     }
 }
--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,24 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public GameObject cubePrefab;
+    public Text scoreText;
 
     float gameLength = 60;
     float turnLength = 2;
     int turn = 0;
     int score = 0;
 
+    int sameColorScore = 10;
+    int rainbowScore = 5;
+    int loseScore = -1;
+
     GameObject[,] grid;
     int gridX = 8;
     int gridY = 5;
     Vector3 cubePos;
 
+    bool gameOver = false;
+
     GameObject nextCube;
-    Vector3 nextCubePos = new Vector3 (8, 10, 0);
+    Vector3 nextCubePos = new Vector3 (7, 10, 0);
     Color[] myColors = { Color.blue, Color.red, Color.green, Color.yellow, Color.magenta };
+    int[] colorBinValue = { 0, 1, 2, 4, 8 };
 
     GameObject activeCube = null;
 
@@ -33,6 +43,8 @@ public class GameController : MonoBehaviour
             {
                 cubePos = new Vector3(x*2, y*2, 0);
                 grid[x,y]= Instantiate(cubePrefab, cubePos, Quaternion.identity);
+                grid[x, y].GetComponent<CubeController>().myX = x;
+                grid[x, y].GetComponent<CubeController>().myY = y;
             }
         }
 
@@ -44,20 +56,27 @@ public class GameController : MonoBehaviour
         {
             nextCube = Instantiate(cubePrefab, nextCubePos, Quaternion.identity);
         }
-        nextCube.GetComponent<Renderer>().material.color = myColors[Random.Range(0, myColors.Length)];
+        int randomColor = Random.Range(0, myColors.Length);
+        nextCube.GetComponent<Renderer>().material.color = myColors[randomColor];
+        nextCube.GetComponent<CubeController>().colorValue = colorBinValue[randomColor];
     }
 
     void EndGame(bool win)
     {
+        //change scene
         //end the game
         if (win)
         {
+            SceneManager.LoadScene("WinScene", LoadSceneMode.Single);
             print("You Win!");
         }
         else
         {
+            SceneManager.LoadScene("LoseScene", LoadSceneMode.Single);
             print("You Lose. Play Again?");
         }
+        gameOver = true;
+        enabled = false;
     }
 
     GameObject FindAvailableCube(int y)
@@ -138,31 +157,98 @@ public class GameController : MonoBehaviour
         }
         print("Placed a black cube!");
     }
-
-
-    void CheckForOneColorPlus()
-    {
-        //checks for plus by scanning through each row and column
-        //adds to score
-        for (int x= 0; x<gridX; x++)
+    
+    void MakeBlackPlus(int x, int y)
+    {if (x== 0 || y==0 || x==gridX-1 || y == gridY - 1) //error check
         {
-            for (int y = 0; y<gridY; y++)
+            return;
+        }
+        grid[x, y].GetComponent<Renderer>().material.color = Color.black;
+        grid[x+1, y].GetComponent<Renderer>().material.color = Color.black;
+        grid[x-1, y].GetComponent<Renderer>().material.color = Color.black;
+        grid[x, y+1].GetComponent<Renderer>().material.color = Color.black;
+        grid[x, y-1].GetComponent<Renderer>().material.color = Color.black;
+
+        grid[x, y].GetComponent<CubeController>().colorValue = 99;
+        grid[x + 1, y].GetComponent<CubeController>().colorValue = 99;
+        grid[x - 1, y].GetComponent<CubeController>().colorValue = 99;
+        grid[x, y + 1].GetComponent<CubeController>().colorValue = 99;
+        grid[x, y - 1].GetComponent<CubeController>().colorValue = 99;
+
+        if (activeCube != null && activeCube.GetComponent<Renderer>().material.color == Color.black)
+        {
+            activeCube.transform.localScale /= 1.5f;
+            activeCube.GetComponent<CubeController>().active = false;
+        }
+    }
+
+    void CheckScore()
+    {
+        
+        for (int x=1; x<gridX-1; x++)
+        {
+            for (int y=1; y<gridY-1; y++)
             {
-                Color tempColor = grid[x, y].GetComponent<Renderer>().material.color;
-                if (grid[x + 1, y].GetComponent<Renderer>().material.color == tempColor && grid[x - 1, y].GetComponent<Renderer>().material.color == tempColor && grid[x, y + 1].GetComponent<Renderer>().material.color == tempColor && grid[x, y - 1].GetComponent<Renderer>().material.color == tempColor)
+                if (CheckForRainbowColorPlus(x, y))
                 {
-                    score += 10;
+                    score += rainbowScore;
+                    MakeBlackPlus(x, y);
+                }
+                if (CheckForOneColorPlus(x, y))
+                {
+                    score += sameColorScore;
+                    MakeBlackPlus(x, y);
                 }
             }
-        }       
+
+        }
+        
+    }
+
+
+    bool CheckForOneColorPlus(int x, int y)
+    {
+        //checks for plus by scanning through each row and column
+        Color tempColor = grid[x, y].GetComponent<Renderer>().material.color;
+        if (tempColor != Color.white && tempColor != Color.black)
+        {
+            if (grid[x + 1, y].GetComponent<Renderer>().material.color == tempColor &&
+                grid[x - 1, y].GetComponent<Renderer>().material.color == tempColor &&
+                grid[x, y + 1].GetComponent<Renderer>().material.color == tempColor &&
+                grid[x, y - 1].GetComponent<Renderer>().material.color == tempColor)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            return false;
+        }
     }
 
     bool CheckForRainbowColorPlus(int x, int y)
     {
         //checks for rainbow
-        Color tempColor = grid[x, y].GetComponent<Renderer>().material.color;
-        score += 5;
-        return true;
+
+        int sum = 0;
+        sum += grid[x + 1, y].GetComponent<CubeController>().colorValue;
+        sum += grid[x - 1, y].GetComponent<CubeController>().colorValue;
+        sum += grid[x, y + 1].GetComponent<CubeController>().colorValue;
+        sum += grid[x, y - 1].GetComponent<CubeController>().colorValue;
+
+        if (sum == 15)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
     }
 
     void ProcessKeyboardInput()
@@ -197,10 +283,10 @@ public class GameController : MonoBehaviour
             PlaceNextCube(numKeyPressed - 1);
         }
     }
-    public void ProcessClick(GameObject clickedCube, int x, int y, Color cubeColor, bool active)
+    public void ProcessClick(GameObject clickedCube, int x, int y, Color cubeColor, bool active, int colorValue)
     {
         //if colored cube, highlight cube
-        if (cubeColor != Color.white && cubeColor != Color.black && nextCube != clickedCube)
+        if (cubeColor != Color.white && cubeColor != Color.black && clickedCube!=nextCube)
         {   //deactivate
             if (active)
             {
@@ -214,36 +300,78 @@ public class GameController : MonoBehaviour
                 {
                     activeCube.transform.localScale /= 1.5f;
                     activeCube.GetComponent<CubeController>().active = false;
+                    clickedCube.transform.localScale *= 1.5f;
+                    clickedCube.GetComponent<CubeController>().active = true;
+                    activeCube = clickedCube;
+
                 }
                 else
                 {
                     clickedCube.transform.localScale *= 1.5f;
-                    clickedCube.GetComponent<CubeController>().active = false;
+                    clickedCube.GetComponent<CubeController>().active = true;
                     activeCube = clickedCube;
                 }
             }
         }
         else if(cubeColor == Color.white)
         {
+            int xDistance = clickedCube.GetComponent<CubeController>().myX - activeCube.GetComponent<CubeController>().myX;
+            int yDistance = clickedCube.GetComponent<CubeController>().myY - activeCube.GetComponent<CubeController>().myY;
 
+            if (Mathf.Abs(yDistance)<= 1 && Mathf.Abs(xDistance) <= 1)
+            {
+                clickedCube.GetComponent<Renderer>().material.color = activeCube.GetComponent<Renderer>().material.color;
+                clickedCube.transform.localScale *= 1.5f;
+                clickedCube.GetComponent<CubeController>().active = true;
+                clickedCube.GetComponent<CubeController>().colorValue = activeCube.GetComponent<CubeController>().colorValue;
+
+                //turn old clicked cube into white cube
+                activeCube.GetComponent<Renderer>().material.color = Color.white;
+                activeCube.transform.localScale /= 1.5f;
+                activeCube.GetComponent<CubeController>().active = false;
+                activeCube.GetComponent<CubeController>().colorValue = 99;
+
+                activeCube = clickedCube;
+            }
         }
     }
+
+
     // Update is called once per frame
     void Update()
     {
-        ProcessKeyboardInput();
-        if (Time.time > turnLength * turn)
+        if (Time.time < gameLength)
         {
-            turn++;
-
-            if(nextCube != null)
+            ProcessKeyboardInput();
+            CheckScore();
+            if (Time.time > turnLength * turn)
             {
-                score -= 1;
-                Destroy(nextCube);
-                PlaceBlackCube();
-            }
-            CreateNextCube();
+                turn++;
 
+                if (nextCube != null)
+                {
+                    score -= loseScore;
+                    if (score < 0)
+                    {
+                        score = 0; //make sure never negative
+                    }
+                    Destroy(nextCube);
+                    PlaceBlackCube();
+                }
+                CreateNextCube();
+            }
+            scoreText.text = "Score: " + score;
+        }
+        else if (!gameOver) //once time runs out, will end game
+        {
+            if (score > 0)
+            {
+                EndGame(true);
+            }
+            else
+            {
+                EndGame(false);
+            }
         }
     }
 }
